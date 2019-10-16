8c6023826916f99d05b3c6eee792507f091b9821
bingwang314 <bingwang_tust@126.com>
2019-10-16 22:36:55 +0800

    补充

--- src/main/java/com/cjs/example/config/MyRoutingDataSource.java
+++ src/main/java/com/cjs/example/config/MyRoutingDataSource.java
@@ -1,8 +1,11 @@
 package com.cjs.example.config;
 
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.lang.Nullable;
 
 public class MyRoutingDataSource extends AbstractRoutingDataSource {
+
+    @Nullable
     @Override
     protected Object determineCurrentLookupKey() {
         return null;
